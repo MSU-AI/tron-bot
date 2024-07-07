cf093164979cb6fc49748536f19510bc2fea224f
gollanai <aidangollan@icloud.com>
2024-07-07 11:10:34 -0400

    tron

--- Game.py
+++ Game.py
@@ -1,89 +1,97 @@
 import pygame
 import numpy as np
-from util import print_grid
 from pygame.locals import *
-from GameObjects import Snake, Apple
+from GameObjects import Snake
 
 class Game:
     def __init__(self, show):
         pygame.init()
-        self.HEIGHT_GRID = 20
-        self.WIDTH_GRID = 20
-        self.HEIGHT = (self.HEIGHT_GRID * 50) - 50
-        self.WIDTH = (self.WIDTH_GRID * 50) - 50        
-        self.score = 0
-        self.fitness = 0
+        self.HEIGHT_GRID = 40
+        self.WIDTH_GRID = 40
+        self.TILE_SIZE = 15
+        self.HEIGHT = self.HEIGHT_GRID * self.TILE_SIZE
+        self.WIDTH = self.WIDTH_GRID * self.TILE_SIZE
         self.font = pygame.font.Font(None, 32)
-        self.surface = pygame.display.set_mode((self.WIDTH_GRID * 50, self.HEIGHT_GRID * 50))
-        self.score_surf = self.font.render(str(self.score), False, (64, 64, 64))
-        self.score_rect = self.score_surf.get_rect(center = (self.WIDTH_GRID * 25, 50))
-        self.snake = Snake(self.surface, self.WIDTH_GRID, self.HEIGHT_GRID)
-        self.apple = Apple(self.surface, self.WIDTH_GRID, self.HEIGHT_GRID)
+        self.surface = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+        self.snake1 = Snake(self.surface, self.WIDTH_GRID, self.HEIGHT_GRID, self.TILE_SIZE, (255, 0, 0))
+        self.snake2 = Snake(self.surface, self.WIDTH_GRID, self.HEIGHT_GRID, self.TILE_SIZE, (0, 0, 255))
         self.clock = pygame.time.Clock()
-        self.directions = {
+        self.directions1 = {
             K_UP: (0, -1),
             K_DOWN: (0, 1),
             K_LEFT: (-1, 0),
             K_RIGHT: (1, 0)
         }
-        self.current_direction = self.directions[K_RIGHT]
+        self.directions2 = {
+            K_w: (0, -1),
+            K_s: (0, 1),
+            K_a: (-1, 0),
+            K_d: (1, 0)
+        }
+        self.current_direction1 = self.directions1[K_RIGHT]
+        self.current_direction2 = self.directions2[K_d]
         self.show = show
-        self.max_frames = 300
-        self.frames = 0
-        self.tick = 3
+        self.tick = 10
 
-    def handle_quit(self):
+    def handle_input(self):
         for event in pygame.event.get():  
             if event.type == QUIT:
                 pygame.quit()
                 quit()
-            elif event.type == KEYDOWN and event.key in self.directions:
-                self.current_direction = self.directions[event.key]
+            elif event.type == KEYDOWN:
+                if event.key in self.directions1:
+                    self.current_direction1 = self.directions1[event.key]
+                elif event.key in self.directions2:
+                    self.current_direction2 = self.directions2[event.key]
 
-    def display_score(self):
-        self.score_surf = self.font.render(str(self.score), False, (64, 64, 64))
-        self.score_rect = self.score_surf.get_rect(center = (self.WIDTH_GRID * 25, 50))
-        self.surface.blit(self.score_surf, self.score_rect)
-
-    def check_wall_col(self):
-        if self.snake.cords[0][0] < 0 or self.snake.cords[0][0] > self.WIDTH:
+    def check_wall_col(self, snake):
+        head = snake.cords[0]
+        if head[0] < 0 or head[0] >= self.WIDTH or head[1] < 0 or head[1] >= self.HEIGHT:
             return True
-        elif self.snake.cords[0][1] < 0 or self.snake.cords[0][1] > self.HEIGHT:
+        return False
+
+    def check_snake_collision(self, snake1, snake2):
+        head1 = snake1.cords[0]
+        head2 = snake2.cords[0]
+        # Check if snake1's head collides with snake2's body
+        if tuple(head1) in set(map(tuple, snake2.cords)):
+            return True
+        # Check if snake2's head collides with snake1's body
+        if tuple(head2) in set(map(tuple, snake1.cords)):
+            return True
+        # Check if snakes' heads collide with each other
+        if head1 == head2:
             return True
         return False
 
     def draw(self):
         self.surface.fill((110, 110, 5))
-        self.apple.draw()
-        self.snake.draw()
-        self.display_score()
+        self.snake1.draw()
+        self.snake2.draw()
         pygame.display.update()
 
     def run(self):
         if self.show:
             self.draw()
         while True:
-            self.handle_quit()
-            self.snake.change_dir(self.current_direction)
-            previous_positions = self.snake.move()
-            if self.check_wall_col() or self.snake.check_self_col(previous_positions):
-                return self.fitness
-            hit = self.apple.check_col(self.snake.cords, self.snake.open_cords)
-            if hit:
-                self.score += 1
-                self.fitness += 100
-                self.frames = 0
+            self.handle_input()
+            self.snake1.change_dir(self.current_direction1)
+            self.snake2.change_dir(self.current_direction2)
+            self.snake1.move()
+            self.snake2.move()
+            
+            if (self.check_wall_col(self.snake1) or self.snake1.check_self_col() or
+                self.check_wall_col(self.snake2) or self.snake2.check_self_col() or
+                self.check_snake_collision(self.snake1, self.snake2)):
+                return "Game Over"
+            
+            self.snake1.update_cords_set()
+            self.snake2.update_cords_set()
+            
             if self.show:
                 self.draw()
                 self.clock.tick(self.tick)
-            if hit:
-                self.snake.add_length()
-            self.fitness -= 1
-            self.frames += 1
-            if self.frames > self.max_frames:
-                return 0
 
-# Example usage:
 if __name__ == "__main__":
     game = Game(show=True)
     game.run()
--- GameObjects.py
+++ GameObjects.py
@@ -1,79 +1,37 @@
 import pygame
 import random
-import copy
-from pygame.locals import *
 
 class Snake:
-    def __init__(self, parent_screen, parent_width, parent_height):
+    def __init__(self, parent_screen, parent_width, parent_height, tile_size, color):
         self.parent_screen = parent_screen
-        cords = (random.randint(0,parent_width - 1) * 50, random.randint(0,parent_height - 1) * 50)
+        self.tile_size = tile_size
+        self.color = color
+        cords = (random.randint(0, parent_width - 1) * tile_size, 
+                 random.randint(0, parent_height - 1) * tile_size)
         self.cords = [list(cords)]
         self.cords_set = {cords}
-        self.directions = [[0,0]]
-        self.length = 1
-        self.block = pygame.image.load("block.png").convert()
-        self.possible_cords = set()
-        for i in range(parent_width - 1):
-            for j in range(parent_height - 1):
-                self.possible_cords.add((i * 50, j * 50))
-        self.open_cords = self.possible_cords - self.cords_set
+        self.directions = [[1, 0]]
 
     def draw(self):
-        for i in range(self.length):
-            self.parent_screen.blit(self.block, (self.cords[i][0], self.cords[i][1]))
+        for cord in self.cords:
+            pygame.draw.rect(self.parent_screen, self.color, 
+                             (cord[0], cord[1], self.tile_size, self.tile_size))
 
     def change_dir(self, dir):
         if not (self.directions[0][0] == -1 * dir[0] and self.directions[0][1] == -1 * dir[1]):
             self.directions[0] = list(dir)
     
-    def update_dir(self):
-        previous_directions = copy.deepcopy(self.directions)
-        for i in range(1, self.length):
-            self.directions[i][0] = previous_directions[i-1][0]
-            self.directions[i][1] = previous_directions[i-1][1]
-    
     def move(self):
-        self.cords_set.clear()
-        previous_positions = copy.deepcopy(self.cords)
-        self.cords[0][0] += self.directions[0][0] * 50
-        self.cords[0][1] += self.directions[0][1] * 50
-        self.cords_set.add(tuple(self.cords[0]))
-        for i in range(1, self.length):
-            self.cords[i][0] = previous_positions[i-1][0]
-            self.cords[i][1] = previous_positions[i-1][1]
-            self.cords_set.add(tuple(self.cords[i]))
-        self.update_dir()
-        self.open_cords = self.possible_cords - self.cords_set
-        return previous_positions
+        new_head = [self.cords[0][0] + self.directions[0][0] * self.tile_size,
+                    self.cords[0][1] + self.directions[0][1] * self.tile_size]
+        self.cords.insert(0, new_head)
+        return self.cords
 
-    def check_self_col(self, previous_positions):
-        for i in range(1, self.length):
-            if self.cords[0] == previous_positions[i]:
-                return True
-        return False
+    def check_self_col(self):
+        return tuple(self.cords[0]) in set(map(tuple, self.cords[1:]))
 
-    def add_length(self):
-        self.cords.append([self.directions[-1][0] * -50, self.directions[-1][1] * -50])
-        self.cords_set.add(tuple(self.cords[-1]))
-        self.directions.append(copy.deepcopy(self.directions)[-1])
-        self.open_cords = self.possible_cords - self.cords_set
-        self.length += 1
+    def get_length(self):
+        return len(self.cords)
 
-class Apple:
-    def __init__(self, parent_screen, parent_width, parent_height):
-        self.parent_screen = parent_screen
-        self.block = pygame.image.load("apple.png").convert()
-        self.cords = (random.randint(0,parent_width - 1) * 50, random.randint(0,parent_height - 1) * 50)
-        self.parent_screen.blit(self.block, (self.cords[0], self.cords[1]))
-
-    def draw(self):
-        self.parent_screen.blit(self.block, (self.cords[0], self.cords[1]))
-
-    def update_pos(self, open_cords):
-        self.cords = random.choice(list(open_cords))
-    
-    def check_col(self, snake_cords, open_cords):
-        col = self.cords == tuple(snake_cords[0])
-        if col:
-            self.update_pos(open_cords)
-        return col
+    def update_cords_set(self):
+        self.cords_set = set(map(tuple, self.cords))
